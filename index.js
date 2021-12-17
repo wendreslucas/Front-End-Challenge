35216b4e552bff88a6ffad4a9a6c5667969f8d59
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2021-12-17 17:12:37 -0300

    color picker ajustado

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const botaoMenu = document.querySelector('.item__icone--menu');
-const menu = document.querySelector('.menu-lateral');
+const botaoMenu = document.querySelector('.item__icone--menu'); // variavel que vai armazenar o botão
+const menu = document.querySelector('.menu-lateral'); // variável que vai tratar o menu inicial.
 
 botaoMenu.addEventListener('click', () => {
-  menu.classList.toggle('menu-lateral--ativo')
+  menu.classList.toggle('menu-lateral--ativo') // função que irá mostrar o menu ativo, "adicionando essa classe no html"
 });
+
+
+const border = document.querySelector('.cartao__conteudo')
+const input = document.querySelector('.projeto__cor')
+
+setColor()
+input.addEventListener('input', setColor)
+
+function setColor() {
+  border.style.background = input.value
+}
