a32501f6d251354cab317cb6fc862c868f953923
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2021-12-23 13:31:16 -0300

    ajuste view port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const botaoMenu = document.querySelector('.item__icone--menu'); // variavel que vai armazenar o botão
-const menu = document.querySelector('.menu-lateral'); // variável que vai tratar o menu inicial.
+const menu = document.querySelector('.menu'); // variável que vai tratar o menu inicial.
 
 botaoMenu.addEventListener('click', () => {
-  menu.classList.toggle('menu-lateral--ativo') // função que irá mostrar o menu ativo, "adicionando essa classe no html"
+  menu.classList.toggle('menu__lateral--ativo') // função que irá mostrar o menu ativo, "adicionando essa classe no html"
 });
 
 
